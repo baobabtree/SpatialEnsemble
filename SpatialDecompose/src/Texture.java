cb03e3ca4465ec107967eebc68302db6671525c4
zhe <zhe@192.168.2.2>
2017-06-14 12:56:01 -0500

    finish textures, to be tested!

--- SpatialDecompose/src/Texture.java
+++ SpatialDecompose/src/Texture.java
@@ -1,12 +1,15 @@
 import java.io.*;
+import java.util.*;
 
 //NOTE: not tested yet!
 public class Texture {
 	
 	//compute texture from input array to output array, pixels are stored row-wise in array
-	public static void ComputeTexture(int input[], int nr, int nc, int w, int numBins, int output[]){
+	public static ArrayList<Double> ComputeTexture(ArrayList<Double> input, int nr, int nc, int w, int numBins){
+		
+		ArrayList<Double> output = new ArrayList<Double>(input.size());
 		int N = nr * nc;
-		if(input.length != N || output.length != N){
+		if(input.size() != N ){
 			System.out.println("Wrong input for Texture, input and output array length not matching nr nc!");
 			System.exit(1);
 		}
@@ -19,8 +22,8 @@ public class Texture {
 		}
 		
 		int ii = 0, jj = 0, ri = 0, ci = 0, l = 0, r = 0, u = 0, d = 0;
-		int widthBin = (int) (256.0 / numBins);
-		int homo = 0;
+		int widthBin = 256 % numBins == 0 ? 256 / numBins : 256 / numBins + 1;
+		double homo;
 		
 		//horizontal direction of texture
 		for(int i = 0; i < N; i++){
@@ -33,10 +36,8 @@ public class Texture {
 			for(ii = u; ii <= d; ii++){
 				for(jj = l; jj <= r; jj++){
 					if( jj < r ){
-						coMat[ input[ii * nc + jj] / widthBin ] [ input[ii * nc + jj + 1] / widthBin ] ++; 
-					}
-					if( jj > l ){
-						coMat[ input[ii * nc + jj] / widthBin ] [ input[ii * nc + jj - 1] / widthBin ] ++;
+						coMat[ (int) (input.get(ii * nc + jj) / widthBin) ] [ (int) (input.get(ii * nc + jj + 1) / widthBin) ] ++; 
+						coMat[ (int) (input.get(ii * nc + jj + 1) / widthBin) ] [ (int) (input.get(ii * nc + jj) / widthBin) ] ++;
 					}
 				}
 			}
@@ -47,7 +48,67 @@ public class Texture {
 					homo += coMat[ii][jj] / (1.0 + Math.abs(ii-jj));
 				}
 			}
-			output[i] = homo;
+			output.add(homo);
 		}
+		
+		return output;
+	}
+	
+	
+	
+	public static ArrayList<ArrayList<Double>> ReadFeaturesIntoArray(String filename, int fDim){
+		ArrayList<ArrayList<Double>> features = new ArrayList<ArrayList<Double>>(fDim);
+		for(int i = 0; i < fDim; i++){
+			features.add(new ArrayList<Double>(10000));
+		}
+		
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ",";
+		try {
+
+			br = new BufferedReader(new FileReader(filename));
+
+			while ((line = br.readLine()) != null) {
+
+			    //use comma as separator
+				String[] fields = line.split(cvsSplitBy);
+				int fld = 0;
+				for(fld = 0; fld < fDim; fld++){
+					features.get(fld).add(Double.parseDouble(fields[fld]));
+				}
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return features;
+	}
+	
+	
+	public static void main(){
+		String filename = "input.txt";
+		int nf = 4;
+		int nr = 111;
+		int nc = 111;
+		int w = 2;
+		int numBins = 32;
+		
+		ArrayList<ArrayList<Double>> features = ReadFeaturesIntoArray(filename, nf);
+		ArrayList<ArrayList<Double>> textures = new ArrayList<ArrayList<Double>>(nf);
+		for(int i = 0; i < nf; i++){
+			textures.add(ComputeTexture(features.get(i), nr, nc, w, numBins));
+		}
+		//print out textures
 	}
 }
